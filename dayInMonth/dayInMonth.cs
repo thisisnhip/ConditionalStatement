854100875b54a0906d9d1d74607516c7f0e74f8a
thisisnhip <nhinguyenphuong2005@gmail.com>
2024-10-14 22:17:31 +0700

    Create dayInMonth.cs

---
+++ dayInMonth/dayInMonth.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap thang ma ban muon dem ngay");
+        int month = int.Parse(Console.ReadLine());
+        string dayInMonth;
+        switch (month)
+        {
+            case 2:
+            dayInMonth = "28 hoac 29";
+            break;
+
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+            dayInMonth = "31";
+            break;
+
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+            dayInMonth = "30";
+            break;
+
+            default:
+            dayInMonth = "";
+            break;
+        }
+
+            if (dayInMonth != "")
+            {
+                System.Console.WriteLine($"Thang {month} co {dayInMonth} ngay");
+            }
+            else
+            {
+                System.Console.WriteLine("Ban nhap sai gia tri");
+            }
+    }
+}
